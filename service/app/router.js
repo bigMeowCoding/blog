14393b51337fe80559d1034043c21e899e6e47f9
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-06 22:28:18 +0800

    api框架初始化

--- service/app/router.js
+++ service/app/router.js
@@ -1,9 +1,9 @@
+const defaultRouter = require('./router/default');
 'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
+  defaultRouter(app);
 };
