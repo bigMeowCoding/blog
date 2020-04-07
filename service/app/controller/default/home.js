a70e4397c63aab2e26f7b8ccd005b785c9ce454d
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-07 09:48:29 +0800

    连接mysql

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -4,8 +4,9 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
     async index() {
-        const { ctx } = this;
-        ctx.body = 'api....';
+        let result = await this.app.mysql.get("blog_content",{})
+        console.log(result)
+        this.ctx.body=result
     }
 }
 
