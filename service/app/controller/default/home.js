05a0f8f172ee75400fe3988d4e815e9e5ed75c93
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-09 00:30:12 +0800

    主页开发

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -7,13 +7,13 @@ class HomeController extends Controller {
         let sql = 'SELECT article.id as id,'+
             'article.title as title,'+
             'article.introduce as introduce,'+
-            'article.addTime as addTime,'+
+            "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
             'article.view_count as view_count ,'+
             'type.typeName as typeName '+
             'FROM article LEFT JOIN type ON article.type_id = type.Id'
 
         const results = await this.app.mysql.query(sql)
-
+debugger
         this.ctx.body={
             data:results
         }
