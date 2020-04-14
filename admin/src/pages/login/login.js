ffca06cab72310f3d49b16f895db248e38b721c3
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-14 15:58:18 +0800

    登陆页面前端

---
+++ admin/src/pages/login/login.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {Button, Card, Input, Spin} from "antd";
+
+import './login.scss'
+import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
+import KeyOutlined from "@ant-design/icons/lib/icons/KeyOutlined";
+
+function Login() {
+    const {Search} = Input;
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
+    return (
+        <div className="login-div">
+
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="JSPang Blog  System" bordered={true} style={{width: 400}}>
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<UserOutlined/>}
+                        onChange={(e) => {
+                            setUserName(e.target.value)
+                        }}
+                    />
+                    <br/><br/>
+
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="Enter your password"
+                        prefix={<KeyOutlined/>}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                    />
+                    <br/><br/>
+                    <Button type="primary" size="large" block onClick={checkLogin}> Login in </Button>
+                </Card>
+            </Spin>
+        </div>
+    )
+}
+
+export default Login
