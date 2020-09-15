14da7099603388790d6d4de35aa21131330bde93
zhouyijun <zhou495248579@hotmail.com>
2020-09-15 21:43:01 +0800

    add favicon and modify footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import '../static/styles/components/footer.css'
 const Footer = ()=>(
     <div className="footer-box">
         <div>系统由 React+Node+Ant Desgin驱动 </div>
-        <div>JSPang.com</div>
+        <div>www.bigmeow.club.com</div>
     </div>
 )
 
