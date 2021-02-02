8c91cfee19917f040bd344893feb5cdf11cd5823
zhouyijun <zhou495248579@hotmail.com>
2021-02-02 20:07:19 +0800

    article list border

--- components/article-list/index.tsx
+++ components/article-list/index.tsx
@@ -9,28 +9,32 @@ interface Prop {
 const ArticleList: FC<Prop> = ({ list }) => {
   return (
     <>
-      {list.map((item) => {
-        return (
-          <div key={item.id} className="mb-3">
-            <Link
-              href={{
-                pathname: `/detail/detail`,
-                query: { id: item.id, typeName: item.typeName },
-              }}
-            >
-              <h3 className="text-2xl mb-4 font-bold text-gray-700">
-                {item.title}
-              </h3>
-            </Link>
-            <div
-              className="text-gray-400 text-sm font-serif italic"
-              dangerouslySetInnerHTML={{
-                __html: marked(item.introduce),
-              }}
-            />
-          </div>
-        );
-      })}
+      <div className='-mb-5'>
+        {list.map((item) => {
+          return (
+              <div key={item.id} className="mb-5 border-b pb-5 border-gray-200">
+                <Link
+                    href={{
+                      pathname: `/detail/detail`,
+                      query: { id: item.id, typeName: item.typeName },
+                    }}
+                >
+                  <h3 className="text-2xl mb-4 font-bold text-gray-700">
+                    {item.title}
+                  </h3>
+                </Link>
+                <div
+                    className="text-gray-400 text-sm font-serif italic"
+                    dangerouslySetInnerHTML={{
+                      __html: marked(item.introduce),
+                    }}
+                />
+              </div>
+          );
+        })}
+      </div>
+
+
     </>
   );
 };
