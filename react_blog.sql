6a7d23459932e90d9cd89c616e521c154a2b7dd6
zhouyijun <zhou495248579@hotmail.com>
2021-02-07 18:16:39 +0800

    modify sql

--- react_blog.sql
+++ react_blog.sql
@@ -26,7 +26,7 @@ CREATE TABLE `admin_user` (
   `password` varchar(255) DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8_general_ci;
 
 -- ----------------------------
 -- Records of admin_user
