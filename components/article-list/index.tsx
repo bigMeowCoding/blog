df6e806c75ed640d45881a8638d6f86342b684ff
zhouyijun <zhou495248579@hotmail.com>
2021-02-19 11:27:13 +0800

    article hover effect

--- components/article-list/index.tsx
+++ components/article-list/index.tsx
@@ -9,32 +9,32 @@ interface Prop {
 const ArticleList: FC<Prop> = ({ list }) => {
   return (
     <>
-      <div className='-mb-5'>
+      <div className="-mb-5">
         {list.map((item) => {
           return (
-              <div key={item.id} className="mb-5 border-b pb-5 border-gray-200">
-                <Link
-                    href={{
-                      pathname: `/detail/detail`,
-                      query: { id: item.id },
-                    }}
-                >
-                  <h3 className="text-2xl mb-4 font-bold text-gray-700">
+            <div key={item.id} className="mb-5 border-b pb-5 border-gray-200  ">
+              <Link
+                href={{
+                  pathname: `/detail/detail`,
+                  query: { id: item.id },
+                }}
+              >
+                <article className=" cursor-pointer">
+                  <h3 className="text-2xl mb-4 font-bold text-gray-700 hover:text-green-500">
                     {item.title}
                   </h3>
-                </Link>
-                <div
-                    className="text-gray-400 text-sm font-serif italic"
+                  <div
+                    className="text-gray-400 text-sm font-serif italic hover:text-green-500"
                     dangerouslySetInnerHTML={{
                       __html: marked(item.introduce),
                     }}
-                />
-              </div>
+                  />
+                </article>
+              </Link>
+            </div>
           );
         })}
       </div>
-
-
     </>
   );
 };
